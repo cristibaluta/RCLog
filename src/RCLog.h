bdf593f62acb81532fb192159a27c0024a105bc6
Baluta Cristian <cristianbal@admins-Mac-mini-4.local>
2015-02-20 17:46:32 +0200

    Added class separation between logs

--- src/RCLog.h
+++ src/RCLog.h
@@ -2,32 +2,31 @@
 //  RCLog.h
 //
 //  Created by Baluta Cristian on 09/10/2013.
-//  Copyright (c) 2013 Baluta Cristian. All rights reserved.
+//  Copyright (c) 2015 Baluta Cristian. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 #define RCLog(fmt, ... ) \
 do { \
-	[RCLog traceFile:[[NSString stringWithUTF8String:__FILE__] lastPathComponent] line:__LINE__ message:[NSString stringWithFormat:(fmt), ##__VA_ARGS__]]; \
+	[RCLog traceFile:[[NSString stringWithUTF8String:__FILE__] lastPathComponent] method:__FUNCTION__ line:__LINE__ message:[NSString stringWithFormat:(fmt), ##__VA_ARGS__]]; \
 } while(0)
 
 #define RCLogO(obj) RCLog(@"%@", obj)
 #define RCLogS(str) RCLog(@"%@", str)
 #define RCLogI(integer) RCLog(@"%i", integer)
+#define RCLogL(long_integer) RCLog(@"%lli", long_integer)
 #define RCLogF(float) RCLog(@"%f", float)
+#define RCLog2F(float) RCLog(@"%.2f", float)
 #define RCLogRect(rect) RCLog(@"%@", NSStringFromCGRect(rect))
 #define RCLogPoint(point) RCLog(@"%@", NSStringFromCGPoint(point))
 #define RCLogSize(size) RCLog(@"%@", NSStringFromCGSize(size))
 #define RCLogThread() RCLog([NSThread isMainThread] ? @"Log from Main Thread":@"Log from Secondary Thread")
-
-//#define rclog(fmt, ... ) RCLog(fmt, ... )
-//#define rclogo(obj) RCLog(@"%@", obj)
-
+#define RCLogTimestamp() RCLog(@"%@", [NSDate date])
 
 @interface RCLog : NSObject
 
 + (void)disableTraces;
-+ (void)traceFile:(NSString *)file line:(int)line message:(NSString *)message;
++ (void)traceFile:(NSString *)file method:(NSString *)method line:(int)line message:(NSString *)message;
 
 @end
--- src/RCLog.m
+++ src/RCLog.m
@@ -10,17 +10,52 @@
 @implementation RCLog
 
 static BOOL _tracesDisabled = NO;
+static NSMutableArray *ALLOW_TRACES_FROM = nil;
+static NSString *lastTracedMethod = @"";
 
-+ (void)traceFile:(NSString *)file line:(int)line message:(NSString *)message {
++ (void)traceFile:(NSString *)file method:(NSString *)methodName line:(int)line message:(NSString *)message {
 	
-	if (!_tracesDisabled) printf("%s:%s: %s\n",
-		   [file cStringUsingEncoding:NSStringEncodingConversionAllowLossy],
-		   [[NSString stringWithFormat:@"%i", line] cStringUsingEncoding:NSStringEncodingConversionAllowLossy],
-		   [message cStringUsingEncoding:NSStringEncodingConversionAllowLossy]);
+    if (ALLOW_TRACES_FROM == nil) {
+        ALLOW_TRACES_FROM = [NSMutableArray array];
+    }
+    if ( ALLOW_TRACES_FROM.count == 0 ) {
+        [self _traceFile:file method:methodName line:line message:message];
+    }
+    else for (NSString *c in ALLOW_TRACES_FROM) {
+        if (c == file) {
+            [self _traceFile:file method:methodName line:line message:message];
+        }
+    }
+}
+
++ (void)_traceFile:(NSString *)file method:(NSString *)methodName line:(int)line message:(NSString *)message {
+    
+    NSString *newLineIn = [lastTracedMethod isEqualToString:methodName] ? @"" : @"\n---> ";
+    NSString *newLineOut = [lastTracedMethod isEqualToString:methodName] ? @"" : @"\n\n";
+    
+//    haxe.Firebug.trace ( inf.methodName + " : " + newLineIn + Std.string(v) + newLineOut, inf );
+    if (!_tracesDisabled) {
+        printf("%s:%s: %s\n",
+               [file cStringUsingEncoding:NSStringEncodingConversionAllowLossy],
+               [[NSString stringWithFormat:@"%i", line] cStringUsingEncoding:NSStringEncodingConversionAllowLossy],
+               [message cStringUsingEncoding:NSStringEncodingConversionAllowLossy]);
+    }
+    
+    lastTracedMethod = methodName;
 }
 
 + (void)disableTraces {
 	_tracesDisabled = YES;
+}
+
+/**!
+ *  Chose the classes you want to see the traces from
+ *  @param arr is an array of strings that represent the class names.
+ *  Call this method as many times as you like.
+ *  If you don't specify any all traces are sent to the output
+ **/
++ (void)allowClasses:(NSArray *)arr {
+    [ALLOW_TRACES_FROM addObjectsFromArray:arr];
 }
 
 @end
