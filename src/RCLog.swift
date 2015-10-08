24a5dd5cd68bca0346b4b6a31a7e45e4593083bb
Baluta Cristian <cristi.baluta@gmail.com>
2015-10-08 06:33:54 +0300

    import problems on osx

--- src/RCLog.swift
+++ src/RCLog.swift
@@ -1,50 +1,52 @@
 //
 //  RCLog.swift
-//  Photography Places
 //
 //  Created by Baluta Cristian on 12/08/2014.
 //  Copyright (c) 2014 Baluta Cristian. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import CoreGraphics
 
-func RCLog (message:String, file:String = __FILE__, line:Int = __LINE__) {
-	println("\(file.lastPathComponent):\(line): \(message)")
+func RCLog (message: String, file: String = __FILE__, line: Int = __LINE__) {
+	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogO (message:AnyObject?, file:String = __FILE__, line:Int = __LINE__) {
-	println("\(file.lastPathComponent):\(line): \(message)")
+func RCLogO (message: AnyObject?, file: String = __FILE__, line: Int = __LINE__) {
+	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogErrorO (message:AnyObject?, file:String = __FILE__, line:Int = __LINE__) {
-	println("**ERROR: \(file.lastPathComponent):\(line): \(message)")
-}
-
-func RCLogI (message:Int?, file:String = __FILE__, line:Int = __LINE__) {
-	println("\(file.lastPathComponent):\(line): \(message)")
-}
-
-func RCLogF (message:Float?, file:String = __FILE__, line:Int = __LINE__) {
-	println("\(file.lastPathComponent):\(line): \(message)")
-}
-
-func RCLogRect (message:CGRect?, file:String = __FILE__, line:Int = __LINE__) {
+func RCLogErrorO (message: AnyObject?, file: String = __FILE__, line: Int = __LINE__) {
 	if message != nil {
-		println("\(file.lastPathComponent):\(line): CGRect\(message!)")
-	} else {
-		println("\(file.lastPathComponent):\(line): \(message)")
+		print("**ERROR: \((file as NSString).lastPathComponent):\(line): \(message)")
 	}
 }
 
-func RCLogSize (message:CGSize?, file:String = __FILE__, line:Int = __LINE__) {
-	println("\(file.lastPathComponent):\(line): \(message)")
+func RCLogI (message: Int?, file: String = __FILE__, line: Int = __LINE__) {
+	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogPoint (message:CGPoint?, file:String = __FILE__, line:Int = __LINE__) {
-	println("\(file.lastPathComponent):\(line): \(message)")
+func RCLogF (message: Float?, file: String = __FILE__, line: Int = __LINE__) {
+	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogThread (file:String = __FILE__, line:Int = __LINE__) {
+func RCLogRect (message: CGRect?, file: String = __FILE__, line: Int = __LINE__) {
+	if message != nil {
+		print("\((file as NSString).lastPathComponent):\(line): CGRect\(message!)")
+	} else {
+		print("\((file as NSString).lastPathComponent):\(line): \(message)")
+	}
+}
+
+func RCLogSize (message: CGSize?, file: String = __FILE__, line: Int = __LINE__) {
+	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+}
+
+func RCLogPoint (message: CGPoint?, file: String = __FILE__, line: Int = __LINE__) {
+	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+}
+
+func RCLogThread (file: String = __FILE__, line: Int = __LINE__) {
 	let message = NSThread.isMainThread() ? "Log from Main Thread" : "Log from Secondary Thread"
-	println("\(file.lastPathComponent):\(line): \(message)")
+	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
