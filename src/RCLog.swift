f83543d94ebbc4d8b94a43297906615b8e05c877
Baluta Cristian <cristi.baluta@gmail.com>
2015-10-24 22:07:20 +0300

    improved logs

--- src/RCLog.swift
+++ src/RCLog.swift
@@ -8,29 +8,33 @@
 import Foundation
 import CoreGraphics
 
-func RCLog (message: String, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+private var tracesDisabled = false
+private var allowedClasses = [String]()
+private var lastTracedMethod = ""
+
+func RCLog (item: Any, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
+	traceFile(file, methodName: function, line: line, message: "\(item)")
 }
 
-func RCLogO (message: AnyObject?, file: String = __FILE__, line: Int = __LINE__) {
-	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+func RCLogO (message: AnyObject?, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
+	traceFile(file, methodName: function, line: line, message: "\(message)")
 }
 
-func RCLogErrorO (message: AnyObject?, file: String = __FILE__, line: Int = __LINE__) {
+func RCLogErrorO (message: AnyObject?, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
 	if message != nil {
 		print("**ERROR: \((file as NSString).lastPathComponent):\(line): \(message)")
 	}
 }
 
-func RCLogI (message: Int?, file: String = __FILE__, line: Int = __LINE__) {
+func RCLogI (message: Int?, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
 	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogF (message: Float?, file: String = __FILE__, line: Int = __LINE__) {
+func RCLogF (message: Float?, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
 	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogRect (message: CGRect?, file: String = __FILE__, line: Int = __LINE__) {
+func RCLogRect (message: CGRect?, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
 	if message != nil {
 		print("\((file as NSString).lastPathComponent):\(line): CGRect\(message!)")
 	} else {
@@ -38,15 +42,54 @@ func RCLogRect (message: CGRect?, file: String = __FILE__, line: Int = __LINE__)
 	}
 }
 
-func RCLogSize (message: CGSize?, file: String = __FILE__, line: Int = __LINE__) {
+func RCLogSize (message: CGSize?, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
 	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogPoint (message: CGPoint?, file: String = __FILE__, line: Int = __LINE__) {
+func RCLogPoint (message: CGPoint?, file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
 	print("\((file as NSString).lastPathComponent):\(line): \(message)")
 }
 
-func RCLogThread (file: String = __FILE__, line: Int = __LINE__) {
+func RCLogThread (file: String = __FILE__, line: Int = __LINE__, function: String = __FUNCTION__) {
 	let message = NSThread.isMainThread() ? "Log from Main Thread" : "Log from Secondary Thread"
 	print("\((file as NSString).lastPathComponent):\(line): \(message)")
+}
+
+
+// MARK: 
+private func traceFile(file: String, methodName: String, line: Int, message: String) {
+	
+	if !tracesDisabled {
+		if allowedClasses.count == 0 {
+			_traceFile(file, methodName: methodName, line: line, message: message)
+		}
+		else {
+	//		for (c in allowedClasses) {
+	//			if (c == (file as NSString).lastPathComponent  [file componentsSeparatedByString:@"."] firstObject]]) {
+	//				_traceFile(file, methodName: methodName, line: line, message: message)
+	//			}
+	//		}
+		}
+	}
+}
+
+private func _traceFile(file: String, methodName: String, line: Int, message: String) {
+	let className = (file as NSString).lastPathComponent
+	let prefix = lastTracedMethod == methodName ? className : "\n\(className):\(methodName)\n\(className)"
+	print("\(prefix):\(line): \(message)")
+    lastTracedMethod = methodName;
+}
+
+func disableTraces() {
+	tracesDisabled = true
+}
+
+/**!
+ *  Chose the classes you want to see the traces from
+ *  @param arr is an array of strings that represent the file names.
+ *  Call this method as many times as you like.
+ *  If you don't specify any all traces are sent to the output
+ **/
+func allowClasses(arr: Array<String>) {
+//    allowedClasses.append(arr)
 }
